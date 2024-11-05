87b265395817d3e0427159df7e6ded34034136e5
Israel Mendoza <artisra@amazon.com>
2024-11-05 01:16:41 -0600

    Improved performance by using an int array instead of a map

--- basicimplementation/valid_anagram.go
+++ basicimplementation/valid_anagram.go
@@ -13,37 +13,35 @@ package basicimplementation
 //
 //	true if `s` and `t` are anagrams, false otherwise.
 //
-// See my solution on LeetCode: https://leetcode.com/problems/valid-anagram/solutions/6009380/beats-100-in-go-time-o-n-space-o-1
+// See my solution on LeetCode: https://leetcode.com/problems/valid-anagram/solutions/6009380/beats-100-in-go-time-o-n-space-o-1.
 func IsAnagram(s string, t string) bool {
 	// Create an array to store the frequency of each letter
 	charFrequency := [26]int{}
 
 	// Count the frequency of each character in string s
 	for _, char := range s {
-		letter := byte(char)
-		charFrequency[charPositionInAlphabet(letter)]++
+		charFrequency[charPositionInAlphabet(char)]++
 	}
 
 	// Decrement the frequency for each character in string t
-	// If any frequency becomes negative, t has more of that character than s
+	// If any frequency becomes negative, t has more of that character than s.
 	for _, char := range t {
-		letter := byte(char)
-		letterPosition := charPositionInAlphabet(letter)
+		letterPosition := charPositionInAlphabet(char)
 		charFrequency[letterPosition]--
 		if charFrequency[letterPosition] < 0 {
 			return false
 		}
 	}
-	// If we've made it through both strings without returning false,
-	// the strings are anagrams
+	// If we made it through both strings without returning false,
+	// the strings are anagrams.
 	return true
 }
 
 // charPositionInAlphabet calculates the zero-based position of a lowercase letter in the alphabet.
 // For example, 'a' returns 0, 'b' returns 1, etc.
 // This function assumes that the input is a lowercase English letter.
-func charPositionInAlphabet(char byte) int {
+func charPositionInAlphabet(char int32) int {
 	// Subtract the ASCII value of 'a' (97) to get the position
-	// 'a' - 'a' = 0, 'b' - 'a' = 1, etc.
+	// 'a' — 'a' = 0, 'b' — 'a' = 1, etc.
 	return int(char) - 97
 }
