254afda8f63a752a0861004b57a9f303bffc5ea2
Israel Mendoza <artisra@amazon.com>
2025-01-23 00:37:03 -0600

    Starting with Top Interview 150

---
+++ topinterview150/merge_sorted_array.go
@@ -0,0 +1,32 @@
+package topinterview150
+
+import "math"
+
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	if n == 0 {
+		return
+	}
+
+	idx1 := m - 1
+	idx2 := n - 1
+
+	for currentIndex := len(nums1) - 1; currentIndex >= 0; currentIndex-- {
+		firstNumber := getFromArray(nums1, idx1)
+		secondNumber := getFromArray(nums2, idx2)
+		if secondNumber >= firstNumber {
+			nums1[currentIndex] = secondNumber
+			idx2--
+		} else {
+			nums1[currentIndex] = firstNumber
+			idx1--
+		}
+	}
+
+}
+
+func getFromArray(array []int, index int) int {
+	if index < 0 {
+		return math.MinInt
+	}
+	return array[index]
+}
---
+++ topinterview150/remove_elements.go
@@ -0,0 +1,14 @@
+package topinterview150
+
+func RemoveElement(nums []int, val int) int {
+	availableIndex := 0
+
+	for _, num := range nums {
+		if num != val {
+			nums[availableIndex] = num
+			availableIndex++
+		}
+	}
+
+	return availableIndex
+}
