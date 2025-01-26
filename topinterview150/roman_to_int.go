4654a56c4e999a01297d931596e355e951b2d3be
Israel Mendoza <artisra@amazon.com>
2025-01-26 01:21:29 -0600

    13. Roman to Integer

---
+++ topinterview150/roman_to_int.go
@@ -0,0 +1,30 @@
+package topinterview150
+
+var romanIntegerMap = map[byte]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
+func RomanToInt(s string) int {
+
+	lastValue := romanIntegerMap[s[len(s)-1]]
+	sum := lastValue
+
+	for i := len(s) - 2; i >= 0; i-- {
+		currentValue := romanIntegerMap[s[i]]
+
+		if currentValue < lastValue {
+			currentValue *= -1
+		}
+
+		sum += currentValue
+		lastValue = currentValue
+	}
+
+	return sum
+}
