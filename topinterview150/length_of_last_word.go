3b4094a7962dd88462fb8ce1f015d1b10be66e29
Israel Mendoza <artisra@amazon.com>
2025-01-26 22:47:50 -0600

    58. Length of Last Word

---
+++ topinterview150/length_of_last_word.go
@@ -0,0 +1,19 @@
+package topinterview150
+
+func LengthOfLastWord(s string) int {
+	foundLetter := false
+	wordLength := 0
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			if foundLetter {
+				break
+			}
+		} else {
+			foundLetter = true
+			wordLength++
+		}
+	}
+
+	return wordLength
+}
